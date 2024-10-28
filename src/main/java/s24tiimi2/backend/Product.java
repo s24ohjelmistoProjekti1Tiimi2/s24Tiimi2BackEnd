529d918ac836e2f3a8fe746bcbc9f5bc25cab8d0
Tiriele <147039063+Tiriele@users.noreply.github.com>
2024-10-28 12:56:21 +0200

    Product olio luotu

--- src/main/java/s24tiimi2/backend/Product.java
+++ src/main/java/s24tiimi2/backend/Product.java
@@ -1,8 +1,78 @@
 package s24tiimi2.backend;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String type;
+    private String color;
+    private String size;
+    private String price;
+    
+    public Product(Long id, String type, String color, String size, String price) {
+        super();
+        this.type = type;
+        this.color = color;
+        this.size = size;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", type=" + type + ", color=" + color + ", size=" + size + ", price=" + price
+                + "]";
+    }
+    
+    
 }

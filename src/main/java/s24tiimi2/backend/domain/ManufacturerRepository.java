f9492464970981a8eb48b4a3b9e3cd83bca7b235
OttoElomaa <otto.elomaa99@gmail.com>
2024-10-28 13:17:26 +0200

    Added Manufacturer Repository

---
+++ src/main/java/s24tiimi2/backend/domain/ManufacturerRepository.java
@@ -0,0 +1,8 @@
+package s24tiimi2.backend.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManufacturerRepository extends CrudRepository<Manufacturer, Long> {
+
+	
+}

de38d75b77e5f72764cea0692c8ae463637a38ed
OttoElomaa <otto.elomaa99@gmail.com>
2024-10-28 13:12:05 +0200

    Added ManyToOne Manufacturer-info to Product

--- src/main/java/s24tiimi2/backend/domain/Product.java
+++ src/main/java/s24tiimi2/backend/domain/Product.java
@@ -1,9 +1,15 @@
 package s24tiimi2.backend.domain;
 
+import java.util.Locale.Category;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Product {
@@ -16,6 +22,12 @@ public class Product {
     private String color;
     private String size;
     private String price;
+
+    @ManyToOne
+    @JsonIgnoreProperties("products")
+    @JoinColumn(name = "manufacturerId")
+    private Manufacturer manufacturer;
+
     
     public Product(Long id, String type, String color, String size, String price) {
         super();
@@ -68,11 +80,22 @@ public class Product {
         this.price = price;
     }
 
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    
     @Override
     public String toString() {
         return "Product [id=" + id + ", type=" + type + ", color=" + color + ", size=" + size + ", price=" + price
                 + "]";
     }
+
+
     
     
 }

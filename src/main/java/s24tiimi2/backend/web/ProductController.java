ffb335965ac22c2a2e18d60bdcd600cdd31764d2
Netta <bhi084@myy.haaga-helia.fi>
2024-10-28 14:34:22 +0200

    Delete product done

--- src/main/java/s24tiimi2/backend/web/ProductController.java
+++ src/main/java/s24tiimi2/backend/web/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import s24tiimi2.backend.domain.Product;
 import s24tiimi2.backend.domain.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
@@ -24,4 +25,11 @@ public class ProductController {
         model.addAttribute("product", new Product());
         return "addproduct";
     }
+
+    @GetMapping("/delete-product/{id}")
+    public String deleteProduct(@PathVariable("id") Long productId, Model model) {
+        repository.deleteById(productId);
+        return "redirect:/productlist";
+    }
+    
 }

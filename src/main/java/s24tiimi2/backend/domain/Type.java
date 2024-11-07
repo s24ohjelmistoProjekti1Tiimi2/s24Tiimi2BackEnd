604fa2725fbf0c13ee900e5484fef3b5270fb1af
matanna <144430369+matanna@users.noreply.github.com>
2024-11-07 15:24:06 +0200

    started type entity

---
+++ src/main/java/s24tiimi2/backend/domain/Type.java
@@ -0,0 +1,55 @@
+package s24tiimi2.backend.domain;
+
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "type")
+    @JsonIgnoreProperties("type")
+    private List<Product> products;
+
+    public Type() {
+    }
+
+    public Type(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
+}

58372d80817c1b8453fda6348c4a11e1665a80ce
OttoElomaa <otto.elomaa99@gmail.com>
2024-11-07 13:39:40 +0200

    Added Rest Controller for Product

---
+++ src/main/java/s24tiimi2/backend/web/ProductRestController.java
@@ -0,0 +1,29 @@
+package s24tiimi2.backend.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import s24tiimi2.backend.domain.Product;
+import s24tiimi2.backend.domain.ProductRepository;
+
+
+
+@RestController
+public class ProductRestController {
+
+	@Autowired
+	private ProductRepository repository;
+
+	// RESTful service to get all products
+	@GetMapping("/api/products")
+	public @ResponseBody List<Product> productListRest() {
+		return (List<Product>) repository.findAll();
+	}
+
+}

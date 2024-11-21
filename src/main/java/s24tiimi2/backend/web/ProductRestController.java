9af5fae825b00cba3cf3e9268b1fbfe8d0d9dbaa
Heidi Ahlgren <heidiahlgren@hotmail.com>
2024-11-21 10:50:13 +0200

    C:/Program Files/Git/api/manufacturers and /api/products added

--- src/main/java/s24tiimi2/backend/web/ProductRestController.java
+++ src/main/java/s24tiimi2/backend/web/ProductRestController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import s24tiimi2.backend.domain.Manufacturer;
+import s24tiimi2.backend.domain.ManufacturerRepository;
 import s24tiimi2.backend.domain.Product;
 import s24tiimi2.backend.domain.ProductRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -17,14 +21,29 @@ import s24tiimi2.backend.domain.ProductRepository;
 public class ProductRestController {
 
 	@Autowired
-	private ProductRepository repository;
+	private ProductRepository productRepository;
 
+	@Autowired
+	private ManufacturerRepository manufacturerRepository;
 
-	// RESTful service to get all products
+	//RESTful service to get all products
+	@GetMapping("/api/products")
+	public @ResponseBody List<Product> getAllProducts() {
+		return (List<Product>) productRepository.findAll();
+	}
+
+	@GetMapping("/api/manufacturers")
+	public @ResponseBody List<Manufacturer> getAllManufacturers() {
+		return (List<Manufacturer>) manufacturerRepository.findAll();
+	}
+	
+	
+
+	// RESTful service to get all products by type name
 	@GetMapping("/api/products/type/{typename}")
 	public @ResponseBody List<Product> productsByTypeRest(@PathVariable String typename) {
 
-		return (List<Product>) repository.findAllByType_Name(typename);
+		return (List<Product>) productRepository.findAllByType_Name(typename);
 	}
 
 }

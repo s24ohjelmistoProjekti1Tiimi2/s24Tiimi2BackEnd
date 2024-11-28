328a43f8c84da402578389d3f400abfd579e13e3
Netta <bhi084@myy.haaga-helia.fi>
2024-11-28 17:15:24 +0200

    Update customer

--- src/main/java/s24tiimi2/backend/domain/CustomerRepository.java
+++ src/main/java/s24tiimi2/backend/domain/CustomerRepository.java
@@ -1,8 +1,7 @@
 package s24tiimi2.backend.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    List<Customer> findByEmail(String email);
+    
 }

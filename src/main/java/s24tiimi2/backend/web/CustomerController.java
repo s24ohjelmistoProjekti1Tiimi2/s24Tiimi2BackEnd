0f5a788023b788d4549a0501ef59564406aa6a70
matanna <144430369+matanna@users.noreply.github.com>
2024-12-02 13:08:29 +0200

    add new customer

--- src/main/java/s24tiimi2/backend/web/CustomerController.java
+++ src/main/java/s24tiimi2/backend/web/CustomerController.java
@@ -3,9 +3,15 @@ package s24tiimi2.backend.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
+import s24tiimi2.backend.domain.Customer;
 import s24tiimi2.backend.domain.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
@@ -17,5 +23,23 @@ public class CustomerController {
     public String getCustomerList(Model model) {
         model.addAttribute("customers", customerRepository.findAll());
         return "customerlist";
+    }
+
+    @GetMapping("/addcustomer")
+    public String addCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "addcustomer";
+    }
+
+    @PostMapping("/savecustomer")
+    public String saveCustomer(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("customer", new Customer());
+            return "addcustomer";
+        }
+        else {
+            customerRepository.save(customer);
+            return "redirect:customerlist";
+        }
     }
 }

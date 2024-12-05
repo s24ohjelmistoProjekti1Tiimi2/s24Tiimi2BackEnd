ca69ffa1e3464da3bbd863021d71dfc1b3e6f103
Netta <bhi084@myy.haaga-helia.fi>
2024-12-05 10:46:44 +0200

    ...

--- src/main/java/s24tiimi2/backend/WebSecurityConfig.java
+++ src/main/java/s24tiimi2/backend/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package s24tiimi2.backend;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/s24tiimi2/backend/web/MyErrorController.java
+++ src/main/java/s24tiimi2/backend/web/MyErrorController.java
@@ -3,16 +3,15 @@ package s24tiimi2.backend.web;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
-    @GetMapping("/error")
+    @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
 
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);

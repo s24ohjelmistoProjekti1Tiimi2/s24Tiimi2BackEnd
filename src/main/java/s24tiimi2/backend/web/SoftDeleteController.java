26f1afa50f94da59489d925eca6d8cab3ad44fbe
matanna <144430369+matanna@users.noreply.github.com>
2024-12-09 00:13:07 +0200

    ready?

--- src/main/java/s24tiimi2/backend/web/SoftDeleteController.java
+++ src/main/java/s24tiimi2/backend/web/SoftDeleteController.java
@@ -1,20 +1,12 @@
 package s24tiimi2.backend.web;
 
-import java.util.Map;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import s24tiimi2.backend.domain.Customer;
 import s24tiimi2.backend.domain.CustomerRepository;
 import s24tiimi2.backend.domain.Manufacturer;
 import s24tiimi2.backend.domain.ManufacturerRepository;

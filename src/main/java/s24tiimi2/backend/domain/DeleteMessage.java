f03084f7e67840ff6e5930be86bb91ae7b804439
OttoElomaa <otto.elomaa99@gmail.com>
2024-12-08 18:36:21 +0200

    Message setters+toString done, MessageController created

--- src/main/java/s24tiimi2/backend/domain/DeleteMessage.java
+++ src/main/java/s24tiimi2/backend/domain/DeleteMessage.java
@@ -18,4 +18,42 @@ public class DeleteMessage {
     private String email;
 
 
+    public DeleteMessage(long id, @NotEmpty String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+
+    public DeleteMessage() {
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DeleteMessage [id=" + id + ", email=" + email + "]";
+    }
+
+
+    
+
 }
---
+++ src/main/java/s24tiimi2/backend/web/MessageController.java
@@ -0,0 +1,8 @@
+package s24tiimi2.backend.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+
+}
